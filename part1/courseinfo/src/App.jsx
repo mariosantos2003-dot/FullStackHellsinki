519c262945913de1cb233d195a00b885a2435343
mariosantos2003-dot <mariosantosbl2003@gmail.com>
2025-04-23 10:23:08 +0200

    1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,26 +2,28 @@ import Header from "./Header";
 import Content from "./Content";
 import Total from "./Total";
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts = {parts} />
+      <Header course={course.name} />
+      <Content parts = {course.parts} />
       <Total exercise1={10} exercise2={7} exercise3={14} />
     </div>
   );
