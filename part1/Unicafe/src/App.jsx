021fb1822eb2c020ae226f9f39aef6e00ad764a1
mariosantos2003-dot <mariosantosbl2003@gmail.com>
2025-04-29 12:40:19 +0200

    1.7 Empezado

--- part1/Unicafe/src/App.jsx
+++ part1/Unicafe/src/App.jsx
@@ -1,22 +1,27 @@
 import { useState } from "react";
 
-
-
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
-  const handleGood = (props) => {
-    setGood(good + 1);
-    console.log(good)
+  const handleGood = () => {
+    console.log(good);
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+    setTotal(updatedGood + neutral + bad);
   };
   const handleNeutral = () => {
-    setNeutral(neutral + 1);
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+    setTotal(good + updatedNeutral + bad);
   };
   const handleBad = () => {
-    setBad(bad + 1);
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    setTotal(good + neutral + updatedBad);
   };
 
   return (
@@ -26,17 +31,18 @@ const App = () => {
       <button onClick={handleNeutral}>Neutral</button>
       <button onClick={handleBad}>Bad</button>
       <div>
-      <h1>statistics</h1> 
-      <p>
-        Good {good}
-        Neutral {neutral}
-        Bad {bad}
-      </p>
-    </div>
-    </div>
+        <h1>statistics</h1>
+        <p>
+          Good {good}
+          Neutral {neutral}
+          Bad {bad}
+          All {total}
 
-   
-
+         Positive
+         Average
+        </p>
+      </div>
+    </div>
   );
 };
 
