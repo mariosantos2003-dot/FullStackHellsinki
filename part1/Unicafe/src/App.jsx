be4a5beb4ef94ee783c6349e39d144c86f6efa8e
mariosantos2003-dot <mariosantosbl2003@gmail.com>
2025-04-29 16:30:36 +0200

    1.8

--- part1/Unicafe/src/App.jsx
+++ part1/Unicafe/src/App.jsx
@@ -1,5 +1,23 @@
 import { useState } from "react";
 
+const Statistics = (props) => {
+  if (props.total === 0) {
+    return (
+      <h1>No FeedBack Given</h1>
+    )
+  }
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good {props.good}</p>
+      <p>Neutral {props.neutral}</p>
+      <p>Bad {props.bad}</p>
+      <p>All {props.total}</p>
+      <p>Positive {props.percentage}</p>
+      <p>Average  {props.average}</p>
+    </div>
+  )
+}
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
@@ -8,7 +26,7 @@ const App = () => {
   const [bad, setBad] = useState(0);
   const [total, setTotal] = useState(0);
   const percentage = (good / total);
-  const average = ((good +1 * 1 + neutral * 0 + bad * -1) / total);
+  const average = ((good + 1 * 1 + neutral * 0 + bad * -1) / total);
 
   const handleGood = () => {
     console.log(good);
@@ -30,7 +48,7 @@ const App = () => {
 
 
   return (
-    
+
     <div>
       <h1>Give Feedback</h1>
       <button onClick={handleGood}>Good</button>
@@ -38,14 +56,14 @@ const App = () => {
       <button onClick={handleBad}>Bad</button>
       <div>
         <h1>statistics</h1>
-        <p>
-          Good {good}
-          Neutral {neutral}
-          Bad {bad}
-          All {total}
-          Positive {percentage}
-          Average  {average}
-        </p>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          percentage={percentage}
+          average={average} />
+
       </div>
     </div>
   );
