ad99c0bb2ec9a11349b45a77e292ab872a713ef1
mariosantos2003-dot <mariosantosbl2003@gmail.com>
2025-04-29 12:59:51 +0200

    1.7.2 done

--- part1/Unicafe/src/App.jsx
+++ part1/Unicafe/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [total, setTotal] = useState(0);
+  const percentage = (good / total);
+
 
   const handleGood = () => {
     console.log(good);
@@ -24,6 +27,8 @@ const App = () => {
     setTotal(good + neutral + updatedBad);
   };
 
+
+
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -37,9 +42,8 @@ const App = () => {
           Neutral {neutral}
           Bad {bad}
           All {total}
-
-         Positive
-         Average
+          Positive {percentage}
+          Average 
         </p>
       </div>
     </div>
